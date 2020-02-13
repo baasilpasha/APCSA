fdce3251809fcc28755c8696d0bb54d6c9512a93
baasilpasha <61026331+baasilpasha@users.noreply.github.com>
2020-02-13 11:35:01 -0800

    Added ZBug and ZBugRunner

---
+++ GridWorldCode/projects/zBug/ZBug.java
@@ -0,0 +1,84 @@
+/* 
+ * AP(r) Computer Science GridWorld Case Study:
+ * Copyright(c) 2005-2006 Cay S. Horstmann (http://horstmann.com)
+ *
+ * This code is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * @author Cay Horstmann
+ * @author Chris Nevison
+ * @author Barbara Cloud Wells
+ */
+
+import info.gridworld.actor.Bug;
+
+/**
+ * A <code>ZBug</code> traces out a square "Z" of a given size. <br />
+ * The implementation of this class is testable on the AP CS A and AB exams.
+ */
+public class ZBug extends Bug
+{
+    private int steps;
+    private final int sideLength;
+    private int count;
+    /**
+     * Constructs a Z bug that traces a square of a given side length
+     * 
+     * @param length the side length
+     */
+    public ZBug(final int length)
+    {
+        steps = 0;
+        sideLength = length;
+        count = 0;
+        turn();
+        turn();
+    }
+
+    /**
+     * Moves to the next location of the square.
+     */
+    public void act(){
+        
+        if (steps < sideLength && canMove())
+        {
+        move();
+        steps++;
+        }
+        
+        else if(count == 0)
+        {
+        turn();
+        turn();
+        turn();
+        steps = 0;
+        count++;
+        }
+        
+        else if(count == 1)
+        {
+        turn();
+        turn();
+        turn();
+        turn();
+        turn();
+        steps = 0;
+        count++;
+        }
+        else if(count == 2)
+        {
+        turn();
+        turn();
+        turn();
+        turn();
+        turn();
+        steps=100000000;
+        }
+}
+}
